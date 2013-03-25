c7d8e36b95709c214a7f2023597e7036a8300f7d
John Slabbekoorn <john.slabbekoorn@gmail.com>
2013-03-25 21:50:24 +0100

    New

---
+++ mr_Exercise1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mr_Exercise1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij versie v0.1 van deze Applicatie");
+            Console.WriteLine("We gaan 2 getallen bij elkaar op te tellen.");
+
+            Console.WriteLine("Voer het eerste getal in:");
+            string userInput1 = Console.ReadLine();
+
+            int userNumber1;
+            userNumber1 = Convert.ToInt32(userInput1);
+
+            Console.WriteLine("Voer het tweede getal in:");
+            string userInput2 = Console.ReadLine();
+
+            int userNumber2;
+            userNumber2 = Convert.ToInt32(userInput2);
+
+            int uitKomst = userNumber1 + userNumber2;
+
+            Console.WriteLine("De uitkomt van de berekening is " + uitKomst);
+            Console.ReadLine();
+        }
+    }
+}
